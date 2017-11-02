02e52d0131e54bd80e3edbf1c744b35753979d25
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2017-11-02 20:01:05 +0300

    Add original maps and a small shellmap

---
+++ mods/raclassic/maps/shellmap/shellmap.lua
@@ -0,0 +1,24 @@
+--[[
+   Copyright 2007-2017 The OpenRA Developers (see AUTHORS)
+   This file is part of OpenRA, which is free software. It is made
+   available to you under the terms of the GNU General Public License
+   as published by the Free Software Foundation, either version 3 of
+   the License, or (at your option) any later version. For more
+   information, see COPYING.
+]]
+
+ticks = 0
+speed = 5
+
+Tick = function()
+	ticks = ticks + 1
+
+	local t = (ticks + 45) % (360 * speed) * (math.pi / 180) / speed;
+	Camera.Position = viewportOrigin + WVec.New(19200 * math.sin(t), 20480 * math.cos(t), 0)
+end
+
+WorldLoaded = function()
+	greece = Player.GetPlayer("Greece")
+	ussr = Player.GetPlayer("USSR")
+	viewportOrigin = Camera.Position
+end
