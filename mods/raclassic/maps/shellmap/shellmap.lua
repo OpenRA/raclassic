1e2fd389c83442cfd592fb91d8fc16fe0d20e8da
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2017-11-06 18:44:40 +0300

    Add stuff to shellmap
    Now they attack to each other

--- mods/raclassic/maps/shellmap/shellmap.lua
+++ mods/raclassic/maps/shellmap/shellmap.lua
@@ -7,6 +7,50 @@
    information, see COPYING.
 ]]
 
+ProducedUnitTypes =
+{
+	{ factory = ABarracks1, types = { "e1", "e1", "e1", "e1", "e3", "e3" } },
+	{ factory = SBarracks1, types = { "e1", "e1", "e1", "e2", "e3", "e3" } },
+	{ factory = SKennel1, types = { "dog" } },
+	{ factory = ANavalYard1, types = { "pt" } },
+	{ factory = SSubPen1, types = { "ss" } }
+}
+
+BindActorTriggers = function(a)
+	if a.HasProperty("Hunt") then
+		Trigger.OnIdle(a, function(a)
+			if a.IsInWorld then
+				a.Hunt()
+			end
+		end)
+	end
+end
+
+ProduceUnits = function(t)
+	local factory = t.factory
+	if not factory.IsDead then
+		local unitType = t.types[Utils.RandomInteger(1, #t.types + 1)]
+		factory.Wait(Actor.BuildTime(unitType))
+		factory.Produce(unitType)
+		factory.CallFunc(function() ProduceUnits(t) end)
+	end
+end
+
+SetupDefensiveUnits = function()
+	Utils.Do(Map.NamedActors, function(a)
+		if (a.Owner == greece or a.Owner == ussr) and a.HasProperty("AcceptsCondition") and a.AcceptsCondition("unkillable") then
+			a.GrantCondition("unkillable")
+			a.Stance = "Defend"
+		end
+	end)
+end
+
+SetupFactories = function()
+	Utils.Do(ProducedUnitTypes, function(production)
+		Trigger.OnProduction(production.factory, function(_, a) BindActorTriggers(a) end)
+	end)
+end
+
 ticks = 0
 speed = 5
 
@@ -21,4 +65,8 @@ WorldLoaded = function()
 	greece = Player.GetPlayer("Greece")
 	ussr = Player.GetPlayer("USSR")
 	viewportOrigin = Camera.Position
+
+	SetupDefensiveUnits()
+	SetupFactories()
+	Utils.Do(ProducedUnitTypes, ProduceUnits)
 end
